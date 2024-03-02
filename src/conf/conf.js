cbba8372043677cb4cbf6e2555e157b2cdefa711
MohdAli664266 <moaliansari664266@gmail.com>
2024-03-02 22:03:36 +0530

    environment variables are wrapped into String class

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,10 +1,9 @@
 const conf = {
-    appwriteUrl: import.meta.env.VITE_APPWRITE_ULR,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    databaseId: import.meta.env.VITE_DATABASE_ID,
-    collectionId: import.meta.env.VITE_COLLECTION_ID,
-    bucketId: import.meta.env.VITE_BUCKET_ID,
-
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_ULR),
+    projectId: String(import.meta.env.VITE_PROJECT_ID),
+    databaseId: String(import.meta.env.VITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    bucketId: String(import.meta.env.VITE_BUCKET_ID),
 }
 
 export default conf;
